dae9151f50dcb6efa9b5237ae26b9900bdc0663f
dimeh95 <mehdowz95331@gmail.com>
2019-12-27 17:16:34 +0100

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Platform } from '@ionic/angular';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
+import { DBMeter } from '@ionic-native/db-meter/ngx';
+import { BatteryStatus } from '@ionic-native/battery-status/ngx';
+import { Sensors, TYPE_SENSOR } from '@ionic-native/sensors/ngx';
+import { Gyroscope, GyroscopeOrientation, GyroscopeOptions } from '@ionic-native/gyroscope/ngx';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -16,7 +20,13 @@ import { AppComponent } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    DBMeter,
+    BatteryStatus,
+    Sensors,
+    Gyroscope,
+    Geolocation,
+    Platform
   ],
   bootstrap: [AppComponent]
 })
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-
+import { Platform } from '@ionic/angular';
+import { DBMeter } from '@ionic-native/db-meter/ngx';
+import { BatteryStatus } from '@ionic-native/battery-status/ngx';
+import { Sensors, TYPE_SENSOR } from '@ionic-native/sensors/ngx';
+import { Gyroscope, GyroscopeOrientation, GyroscopeOptions } from '@ionic-native/gyroscope/ngx';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { Platform } from '@ionic/angular';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +13,63 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private platform: Platform,private dbMeter: DBMeter,private batteryStatus: BatteryStatus,private geolocation: Geolocation,private gyroscope: Gyroscope,private sensors: Sensors) {platform.ready().then(() => {});}
+
+  getD() {
+
+    let subscription = this.dbMeter.start().subscribe(
+    data => console.log(data)
+  );
+  subscription.unsubscribe();
+
+  }
+
+
+  getB() {
+    const subscription = this.batteryStatus.onChange().subscribe(status => {
+   console.log(status.level, status.isPlugged);
+});
+
+  }
+
+  getAll() {
+    try {
+      this.sensors.enableSensor(TYPE_SENSOR.LIGHT);
+
+}
+catch(error) {
+  console.error("erreur ios");
+}
+
+let subscription = this.dbMeter.start().subscribe(
+data => console.log(data)
+);
+subscription.unsubscribe();
+
+
+const subscription1 = this.batteryStatus.onChange().subscribe(status => {
+console.log(status.level, status.isPlugged);
+});
+subscription1.unsubscribe();
+try {
+  var promise1 = Promise.resolve(this.sensors.getState());
+
+promise1.then(function(value) {
+  console.log(value);
+  // expected output: 123
+});
+}
+catch(error) {
+console.error("null");
+}
+
+
+
+
+  }
+
+
+
+
 
 }
